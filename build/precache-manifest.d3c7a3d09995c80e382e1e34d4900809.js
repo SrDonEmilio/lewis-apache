e3479052f14406b0b1e48d3442c65a61dae9a545
SrDonEmilio <emilio@nextup.dev>
2020-05-17 02:26:19 -0500

    Project migrated to React with Reco(react.cordova package)

---
+++ build/precache-manifest.d3c7a3d09995c80e382e1e34d4900809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55732d96a0fb1166a5a7ce87bb3be898",
+    "url": "./index.html"
+  },
+  {
+    "revision": "858584928bc588cb9e39",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "672423f2474a1d08b2a7",
+    "url": "./static/js/2.e771a178.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e771a178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858584928bc588cb9e39",
+    "url": "./static/js/main.f5076dfb.chunk.js"
+  },
+  {
+    "revision": "0cd6ac89f40fbdfb460d",
+    "url": "./static/js/runtime-main.bb104862.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
